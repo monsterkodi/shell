f89d6eba811ac281840776426efa7f3b57d2736e
monsterkodi <monsterkodi@gmx.net>
2015-05-17 22:53:41 +0200

    apm

---
+++ setup/apm.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+apm install stylus
+apm install aligner
+apm install aligner-css
+apm install aligner-scss
+apm install atom-beautify
+apm install autocomplete-paths
+apm install build
+apm install file-icons
+apm install filetype-color
+apm install tree-view-filter
+apm install language-fish-shell
+apm install minimap
+apm install preview
+apm install project-manager
+apm install sort-lines
+apm install zentabs
+apm install toggle-invisibles
+
+apm install even-darker-syntax
+apm install flatland-dark-ui
